de0b5d666b4967b2804a2bd4f42b782427b22c25
achao <zhangshichao628@sina.com>
2017-07-25 17:11:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">角色</template>
+        <el-menu-item index="2-1" >zyyyyy</el-menu-item>
+      </el-submenu>
+    </el-menu>
     <router-view></router-view>
   </div>
 </template>
